07d2c6e7d6b27d73b28f8a0499700edab389b27b
林沐宇 <1243935366@qq.com>
2022-07-22 11:08:20 +0800

    填充每一个节点的下一个右侧节点指针

--- src/com/sherlock/utils/TreeNode.java
+++ src/com/sherlock/utils/TreeNode.java
@@ -9,6 +9,7 @@ public class TreeNode {
     public Integer val;
     public TreeNode left;
     public TreeNode right;
+    public TreeNode next;
     public TreeNode() {}
     public TreeNode(Integer val) { this.val = val; }
     public TreeNode(Integer val, TreeNode left, TreeNode right) {
